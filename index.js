1ec4ad46c97740a44ea1d0be2733f9785d50d4f8
Francois Laubscher <francoislaubscher@outlook.com>
2016-03-20 13:32:36 +0200

    fixed bug on unix filesystems

--- index.js
+++ index.js
@@ -28,13 +28,13 @@ var resizeImage = function(option, width, height, stream, next) {
 
 module.exports = {
     resize: function(option, width, height, url, next) {
-        var path = `${__dirname}\\${url}`;
-        path = path.replace('\\node_modules\\node-imager', '');
+        var filename = `${__dirname}/${url}`;
+        filename = filename.replace('/node_modules/node-imager', '');
         var download = false;
 
         // check if file exists locally        
         try {
-            fs.accessSync(path);
+            fs.accessSync(filename);
         } catch (ex) {
             download = true;
         }
@@ -43,9 +43,9 @@ module.exports = {
 
             if (!download) {
                 // file is on server
-                var stream = fs.readFileSync(path);
+                var stream = fs.readFileSync(filename);
                 resizeImage(option, width, height, stream, (image) => {
-                    var type = mime.lookup(path);
+                    var type = mime.lookup(filename);
                     next(type, image);
                 });
 
